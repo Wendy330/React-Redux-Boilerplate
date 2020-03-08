27a7c5cb226560bfa2a6834d5e009b9730f9fe3b
Yawen Qian <qian91328@gmail.com>
2020-03-07 20:00:08 -0600

    Add reducer

---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,28 @@
+export default function () {
+    return [
+        {
+            id: 1,
+            first: "Hebe",
+            last: "Tien",
+            age: 30,
+            description: "Hebe is a the best singer",
+            thumbnail: "http://i.imgur.com/7yUvePI.jpg"
+        },
+        {
+            id: 2,
+            first: "Fuzhen",
+            last: "Tien",
+            age: 330,
+            description: "Hebe is a the best singer",
+            thumbnail: "http://i.imgur.com/7yUvePI.jpg"
+        },
+        {
+            id: 3,
+            first: "Hebe",
+            last: "Tien",
+            age: 37,
+            description: "Hebe is a the best singer",
+            thumbnail: "http://i.imgur.com/7yUvePI.jpg"
+        }
+    ]
+}
